cfe873ec375b08385ca5294275644059ca55d138
fxcat <sumoyu@droi.com>
2017-11-16 22:51:08 +0800

    complete ajax

--- WDK.BStd/source/mio-ajax.js
+++ WDK.BStd/source/mio-ajax.js
@@ -16,11 +16,11 @@
   //try create IE request
   var winReqTry = () => {
     return _f.except(() => {
-      var try = new glob.ActiveXObject("Msxml2.XMLHTTP");
+      var tryc = new glob.ActiveXObject("Msxml2.XMLHTTP");
       return () => new glob.ActiveXObject("Msxml2.XMLHTTP");
     })((e)=>{
       _f.except(() => {
-        var try = new glob.ActiveXObject("Microsoft.XMLHTTP");
+        var tryc = new glob.ActiveXObject("Microsoft.XMLHTTP");
         return () => new glob.ActiveXObject("Microsoft.XMLHTTP");
       })((e)=> null)()
     })();
@@ -35,9 +35,11 @@
     return;
   }
 
+  //parameter codec
   var reqCoder = {
-    "json" : {
+    "json" : {  //JSON codec
       "type":"application/json",
+      "dataclass":"raw",
       "coder": (params) => {
         var obj = {};
         var keyexist = {};
@@ -55,8 +57,9 @@
         return JSON.stringify(obj);
       }
     },
-    "uri" : {
+    "uri" : { //form URI codec
       "type":"application/x-www-form-urlencoded",
+      "dataclass":"uri",
       "coder": (params) => {
         return _f.list((pf) => {
           return pf((k,v)=>{
@@ -65,50 +68,179 @@
         })(_f.iList)(params).join("&")
       }
     }
-  }
+  };
 
   //Request data module
   var dataReq = function(){
     var mod = {};
-    mod.header = [];
-    mod.method = null;
+    var parameter = [];
+    var header = [];
+    var onsuccess = [];
+    var onerror = [];
+    var onstate = [];
+    var paramraw = null;
+    var paramcodc = reqCoder.uri.coder;
+    var paramclass = reqCoder.uri.dataclass;
+
+    mod.method = "GET";
     mod.async = true;
-    mod.parameter = [];
-    mod.onsuccess = [];
-    mod.onerror = [];
-    mod.onstate = [];
-    mod.rawdata = null;
-    //prepare HTTP hander
-    mod.addHeader = (key, val) => {
-      key = String(key);
-      val = String(val);
-      mod.header.push((req) => {
-        req.setRequestHeader(key, val)
-      });
+    mod.timeout = 30;
+    mod.cnttype = reqCoder.uri.type;
+    mod.user = null;
+    mod.passwd = null;
+
+    //prepare RAW data
+    mod.setRaw = (raw) => {
+      function rawStr(data){
+        paramraw = data;
+      };
+      function rawObj(data){
+        paramraw = JSON.stringfy(data);
+        mod.cnttype = "application/json";
+      };
+      function rawNone(){ 
+        paramraw = null;
+      };
+      typeof(raw) == "string" ? rawStr(raw) : (raw === null ? rawNone() : rawObj(raw));
     };
+    mod.setParamCodec = (name) => {
+      if (!reqCoder[name]) return false;
+      paramcodc = reqCoder[name].coder;
+      paramclass = reqCoder[name].dataclass;
+      mod.cnttype = reqCoder[name].type;
+      return true;
+    }
     //prepare HTTP parameter
     mod.addParameter = (key, val) => {
       key = String(key);
       val = String(val);
-      mod.header.push((creator) => creator(key, val));
-    }
+      parameter.push((creator) => creator(key, val));
+    };
+    //prepare HTTP hander
+    mod.addHeader = (key, val) => {
+      if (key.toLowerCase() == "content-type"){
+        mod.cnttype = val;
+        return;
+      }
+      key = String(key);
+      val = String(val);
+      header.push((req) => req.setRequestHeader(key, val));
+    };
     //add success listener
+    //successhandle = (content, http_code)
     mod.addSuccess = (handle) => {
       onsuccess.push(handle)
-    }
+    };
     //add error listener
+    //errorhandle = (http_code) => {}
     mod.addError = (handle) => {
       onerror.push(handle)
     }
     //add state listener
+    //statehandle = (ready_code, get_header, send_more) => {}
     mod.addState = (handle) => {
       onstate.push(handle)
+    };
+    //call state event
+    var cbState = (readycode, req) => {
+      var getheader = (name) => req.getResponseHeader(name);
+      return onstate.length < 1 || _f.reduce(
+        (a,b) => a && b, (evstate) => evstate(readycode, getheader))(_f.iList)(onstate);
     }
+    //call state event
+    var cbError = (code) => {
+      _f.each((everror) => everror(code))(_f.iList)(onerror);
+    };
+    //call state event
+    var cbSuccess = (content, code) => {
+      _f.each((evsuccess) => evsuccess(content, code))(_f.iList)(onsuccess);
+    };
+    //reqire complete processor
+    var stateComplete = (req) => {
+      var httpstatus = req.status;
+      if (Math.floor(httpstatus / 100) != 2) cbError(httpstatus);
+      else cbSuccess(httpstatus.responseText, httpstatus);
+    };
+    //default state change processor
+    var defaultState = (statecode ,req) => statecode == 4 && stateComplete(req);
+
+    //Open url request
+    var openURL = (req) => {
+      var baseOpen = (method, url) => {
+        var args = [method, url, mod.async]
+        if (mod.user) args.push(mod.user);
+        if (mod.passwd) args.push(mod.passwd);
+        req.timeout = mod.timeout || 0;
+        req.open.apply(req, args);
+        _f.each((prochead) => prochead(req))(_f.iList)(header);
+        req.setRequestHeader("Content-Type",mod.cnttype);
+        return true;
+      }
+      //process addit parameter or RAW
+      var procAdditData = (afterProc) => {
+        var data = mod.paramraw || (parameter.length && paramcodc(parameter));
+        return afterProc ? afterProc(data) : data;
+      }
+      //require with http-content
+      var withContent = (method, url) => {
+        return baseOpen(method, url) && (()=>{
+          var data = procAdditData();
+          if (data){
+            req.send(data);
+            req.send(null);
+            return true;
+          }else{
+            var sender = (data) => {
+              if (req.readyState >= 3) return false;
+              req.send(data);
+              return data === null ? true : sender;
+            }
+            return sender;
+          }
+        })();
+      }
+      //require without http-content
+      var withoutContent = (method, url) => {
+        var psplit = () => {
+          var paramtest = /.+\?.*/;
+          var paramnone = /.+\?$/;
+          paramtest.test(url) ? (paramnone.test(url) ? "" : "&") : "?";
+        }
+        var pconv = paramclass == "uri" ? ((data) => data) : ((data) => encodeURIComponent(data));
+        var url = procAdditData((data) => url + (data ? psplit() + pconv(data) : ""));
+        return baseOpen(method, url) && (() => {
+          req.send(null);
+          return true;
+        })();
+      }
+      return {
+        "GET":(url) => withoutContent("GET"),
+        "DELETE":(url) => withoutContent("DELETE"),
+        "POST":(url) => withContent("POST"),
+        "PUT":(url) => withContent("PUT")
+      }
+    }
+
     //create Request
     mod.req = (url) => {
-    }
+      var req = Requester();
+      //ready state change
+      req.onreadystatechange = () => {
+        var state_code = req.readyState;
+        if (cbState(state_code, req) === false){
+          req.abort();
+          return;
+        }
+        defaultState(state_code, req);
+      }
+      var opener = openURL(req)
+      return opener[mod.method] && opener[mod.method](url);
+    };
 
     //export module
     return mod;
   }
+
+  //append to glob
+  _f.ajax = dataReq;
 })();
